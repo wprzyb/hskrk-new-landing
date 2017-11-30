18d0bd51af74fc4d11029634be62ddc8bbe758bd
Wiktor Przybylski <wikop1@gmail.com>
2017-11-30 16:24:22 +0100

    scripts folder is deprecated - since we don't use that scripts and we used them during the wordpress age ;v

--- scripts/makapaka.js
+++
@@ -1,9 +0,0 @@
-jQuery(function ($) {
-    cheet('up up down down left right left right b a', function () {
-        jQuery('#middle .container').addClass('makapakad');
-	console.log('Makapaka!!')
-        window.setTimeout(function() {
-            jQuery('#middle .container').removeClass('makapakad');
-        }, 3000);
-    });
-});
